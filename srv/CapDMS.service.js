3b997d88b4c5e3f9b8e88e4d8a110ae8d0425cf1
Gaurav Surwade <gsurwade@deloitte.com>
2025-09-22 20:13:07 +0000

    saving

--- srv/CapDMS.service.js
+++ srv/CapDMS.service.js
@@ -16,82 +16,82 @@ class CapDMS extends cds.ApplicationService {
             })
         });
         this.on('uploadToDMS', async (req) => {
-            let db = await cds.connect.to('db')
-            const base64Data = req.data.content; // LargeBinary
-            const buffer = Buffer.from(base64Data, 'base64');
-            const workbook = XLSX.read(buffer, { type: 'buffer' });
-            const sheet = workbook.Sheets[workbook.SheetNames[0]];
-            const rows = XLSX.utils.sheet_to_json(sheet);
-            const repositoryId = process.env.REPOSITORY_ID;
+            try {
 
-            //create request
-            const sRequestID = GuidUtil.createGuid();
 
-            //upload file to dms
-            const oToken = await Util.getSDMToken();
-            //create folder
-            let rootPath = "";
-            const createdFolder = await Util.createFolder(oToken.sdmEndPoint, oToken.token, repositoryId, rootPath, sRequestID);
+                let db = await cds.connect.to('db')
+                const base64Data = req.data.content; // LargeBinary
+                const buffer = Buffer.from(base64Data, 'base64');
+                const workbook = XLSX.read(buffer, { type: 'buffer' });
+                const sheet = workbook.Sheets[workbook.SheetNames[0]];
+                const rows = XLSX.utils.sheet_to_json(sheet);
+                const repositoryId = process.env.REPOSITORY_ID;
 
-            //upload ECON file to folder
-            if (createdFolder?.data?.succinctProperties["cmis:name"] && createdFolder.status === 201) {
-                const parentId = "";
-                const path = `/${createdFolder.data.succinctProperties["cmis:name"]}`;
-                const createdDocument = await Util.createDocument(req.data, oToken.sdmEndPoint, oToken.token, parentId, repositoryId, path);
-                if (createdDocument?.data?.succinctProperties["cmis:objectId"] && createdDocument.status === 201) {
-                    //Creating data in HANA DB
-                    await db.tx(req).run(INSERT.into('db.Request').entries({
-                        RequestID: sRequestID,
-                        process: "MAIN"
-                    }));
-                    //ECON file details saving to HANA DB
-                    await db.tx(req).run(INSERT.into('db.FileAttachment').entries({
-                        RequestID: sRequestID,
-                        process: "MAIN",
-                        path: path,
-                        repository: repositoryId,
-                        fileName: req.data.fileName ? req.data.fileName : "ProcessFile.xlsx",
-                        objectId: createdDocument?.data?.succinctProperties["cmis:objectId"],
-                        folderId: createdFolder?.data?.succinctProperties["cmis:name"],
-                        isSubProcess: false,
-                    }));
+                //create request
+                const sRequestID = GuidUtil.createGuid();
 
-                    const aEntries = rows.slice(1).map(obj => ({
-                        RequestID: sRequestID,
-                        processName: obj["__EMPTY_4"],
-                        code: obj["Level 4"],
-                        isLinkedToCloudALM: false,
-                        level: "L4"
-                    }));
-                    await db.tx(req).run(INSERT.into('db.Process').entries(aEntries));
+                //upload file to dms
+                const oToken = await Util.getSDMToken();
+                //create folder
+                let rootPath = "";
+                const createdFolder = await Util.createFolder(oToken.sdmEndPoint, oToken.token, repositoryId, rootPath, sRequestID);
 
+                //upload ECON file to folder
+                if (createdFolder?.data?.succinctProperties["cmis:name"] && createdFolder.status === 201) {
+                    const parentId = "";
+                    const path = `/${createdFolder.data.succinctProperties["cmis:name"]}`;
+                    const createdDocument = await Util.createDocument(req.data, oToken.sdmEndPoint, oToken.token, parentId, repositoryId, path);
+                    if (createdDocument?.data?.succinctProperties["cmis:objectId"] && createdDocument.status === 201) {
+                        //Creating data in HANA DB
+                        await db.tx(req).run(INSERT.into('db.Request').entries({
+                            RequestID: sRequestID,
+                            process: "MAIN"
+                        }));
+                        //ECON file details saving to HANA DB
+                        await db.tx(req).run(INSERT.into('db.FileAttachment').entries({
+                            RequestID: sRequestID,
+                            process: "MAIN",
+                            path: path,
+                            repository: repositoryId,
+                            fileName: req.data.fileName ? req.data.fileName : "ProcessFile.xlsx",
+                            objectId: createdDocument?.data?.succinctProperties["cmis:objectId"],
+                            folderId: createdFolder?.data?.succinctProperties["cmis:name"],
+                            isSubProcess: false,
+                        }));
+
+                        const aEntries = rows.slice(1).map(obj => ({
+                            RequestID: sRequestID,
+                            processName: obj["__EMPTY_4"],
+                            code: obj["Level 4"],
+                            isLinkedToCloudALM: false,
+                            level: "L4"
+                        }));
+                        await db.tx(req).run(INSERT.into('db.Process').entries(aEntries));
+                        return {
+                            "isFileSaved": true,
+                            "isProcessCreated": true,
+                            "RequestID": sRequestID
+                        }
+                    } else {
+                        req.error({
+                            code: 512,
+                            message: "Could not not create document"
+                        })
+                    }
+                } else {
+                    req.error({
+                        code: 512,
+                        message: "Could not not create folder"
+                    });
                 }
-            } else {
-                throw new Error("Could not create folder")
-            }
-            //create Process in Table
-
-            //return Request ID in response
 
 
-            // const { tenant, metadata } = req.data;
-            // const subdomain = metadata?.subscribedSubdomain;
-            // const SDMCredentials = cds.env.requires?.sdm?.credentials;
-            // const sdmUrl = SDMCredentials?.uri;
-            // const repository = buildRepositoryObject();
-            // const token = await fetchSDMToken(subdomain, SDMCredentials.uaa);
-            // Example: Update child tables
-            // const tx = cds.transaction(req);
-            // for (const row of rows) {
-            //     await tx.run(
-            //         INSERT.into('MyChildTable').entries(row)
-            //     );
-            // }
-            // return { success: true, count: rows.length };
-            return {
-                "isFileSaved": false,
-                "isProcessCreated": false,
-                "message": "Hi"
+
+            } catch (error) {
+                req.error({
+                    code: 500,
+                    message: Err.message ? Err.message : "Internal Server Error"
+                });
             }
         });
 
